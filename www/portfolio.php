cb164b21ae28ebf8ebe949bfd353caa91dffef41
Gavrilov Stanislav <stanislav-09@inbox.ru>
2017-02-18 00:42:16 +0400

    Add files via upload

---
+++ www/portfolio.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8"/>
+		<title>St_animation</title>
+		<link href="css/style.css" rel="stylesheet" type="text/css"/>
+		<link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon"/>
+		<script type="text/javascript" src="scripts/jquery-3.1.1.min.js" ></script>	
+	</head>
+	<body bgcolor="#F5F5F5">
+		<div id="site">
+			<div id="info">
+			<!--------------------------Портфолио----------------
+			--------------------------------------------------->
+			<div id="main">
+					<h2 class="heading" title='Портфолио'>Портфолио</h2>
+			</div>
+			<!--------------------------Конец раздела Портфолио----------------
+			--------------------------------------------------->
+			
+			<!--------------------------Галерея----------------
+			--------------------------------------------------->
+			<div id="galarey">
+				<!--<ul div="gal">
+					<div class="li1">
+						<li style="bacground-image: url('images/1.png')"></li>
+						<li style="bacground-image: url('images/2.png')"></li>
+						<li style="bacground-image: url('images/3.png')"></li>
+					</div>
+				</ul>-->
+				<button id="prev" title='Предыдущая картинка'>prev</button>
+				<button id="next" title='Следующая картинка'>next</button>
+			</div>
+			<!--------------------------конец раздела Галерея----------------
+			--------------------------------------------------->
+			</div>	
+			
+				<div id="gl">
+					<a href="index.php" title="Главная">
+							<div id="btn">
+								Главная
+							</div>
+					</a>
+				</div>
+		</div>
+	</body>
+</html>
